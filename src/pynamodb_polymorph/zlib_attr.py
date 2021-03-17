894f02597d84a225e4125354803c1a80be2917dc
Ryan Scott Brown <rsb@vendia.net>
2021-03-17 12:29:29 -0400

    .2.1 release

---
+++ src/pynamodb_polymorph/zlib_attr.py
@@ -0,0 +1,23 @@
+import zlib
+
+from pynamodb.attributes import Attribute
+
+
+class CompressedAttribute(Attribute[bytes]):
+    """
+    A zlib-compressed binary attribute
+    """
+
+    attr_type = BINARY
+
+    def serialize(self, value):
+        """
+        Returns the compressed binary of the value
+        """
+        return zlib.compress(value)
+
+    def deserialize(self, value):
+        """
+        Returns a decoded byte string from a base64 encoded value
+        """
+        return zlib.decompress(value)
